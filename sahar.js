bba2bff0016ad2bfc7dade162581b2fd62ae5d1f
Shehri8 <107769028+Shehri8@users.noreply.github.com>
2022-06-18 21:16:32 +0300

    Add files via upload

---
+++ sahar.js
@@ -0,0 +1,8 @@
+var myText = document.getElementById('text');
+ var myBtn = document.getElementById('btn');
+ var myText2 = document.getElementById('text2');
+
+ myBtn.onkeydowns = function console.warn();
+ {
+myBtn.innerHTML= Document.myText2.value;
+ }
